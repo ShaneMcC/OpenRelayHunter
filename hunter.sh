a6ad71b27d9660e2738036a91854372999a15df2
root <root@bumblebee.suitedupgeek.co.uk>
2016-07-06 17:13:06 +0100

    added email code, added recursive dns checker, added prips command instead of an nmap extrapolation for IP ranges

--- hunter.sh
+++ hunter.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
+# 
+# Quick and dirty script to scan a selection of IP ranges for Open SMTP or DNS resolvers
+# Prerequisite: 'prips' utility (aptitude install prips) 
+# Prerequisite: 'mutt' utility (aptitude install mutt) 
+# Author: Johnathan Williamson
+# Props: Shane McCormack (prips, open dns resolver snippet) 
+# 
 
-touch /tmp/ips.txt
 touch /tmp/badservers.txt
 
 # These are the IP Ranges we'll be hunting through (note this will work with public or private IP ranges.
-IPADDR=("192.168.0.0/24")
+IPADDR=("192.168.0.0/30")
 
 # Iterate through each range and push the IP's into a new array
 for i in "${IPADDR[@]}"
 	do
 	# Expand our ranges to list the full IP range
-	nmap -sL $i | grep "Nmap scan report" | tr -d '(' | tr -d ')' | awk '{print $NF}' > /tmp/ips.txt
+
 		#Check each IP for open relay status
-		for line in $(cat /tmp/ips.txt)
+		for line in `prips $i`
 			do
-			if [ -z "$(nmap --script smtp-open-relay $line | grep "Server is an open relay")" ]; 
+			echo Checking $line
+			if [ -z "$(nmap -pT:25 --script smtp-open-relay $line | grep "Server is an open relay")" ]; 
 				then
 					true	
 				else 
-					echo "$line is an open relay. Burn them with unholy bleach." >> /tmp/badservers.txt
+					echo "$line is an open SMTP relay. Burn them with unholy bleach." >> /tmp/badservers.txt
 			fi
+
+			if [ "$(dig +time=1 +tries=1 +short google.com @$line | grep -v ';;')" != "" ];
+				then echo "$line is an open DNS Resolver. Burn them with even unholier bleach." >> /tmp/badservers.txt
+			else
+				true
+			fi
+			
 		done
 done
 
 # Did we catch anybody? If yes, email somebody to do something about them.
 if [[ $(find /tmp/badservers.txt -type f -size +1c 2>/dev/null) ]]; then
-	cat /tmp/badservers.txt
+	mail -r "From@youraddress.com" -s "List of Open DNS and SMTP Servers" "your@recipient.com" < /tmp/badservers.txt
+
 fi
 
-rm -rf /tmp/ips.txt
 rm -rf /tmp/badservers.txt
